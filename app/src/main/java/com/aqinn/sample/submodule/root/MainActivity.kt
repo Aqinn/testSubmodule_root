9aea83a02648f128e01c1a435be6ed0923e3620f
allanzhong <allanzhong@tencent.com>
2023-03-28 16:20:02 +0800

    update

--- app/src/main/java/com/aqinn/sample/submodule/root/MainActivity.kt
+++ app/src/main/java/com/aqinn/sample/submodule/root/MainActivity.kt
@@ -2,10 +2,19 @@ package com.aqinn.sample.submodule.root
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btTest: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btTest = findViewById(R.id.bt_test)
+        btTest.setOnClickListener {
+
+        }
     }
 }
